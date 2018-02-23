4d83baee0f73aedbc012b86884b0267812d09d82
aljoh <reachingalfred@gmail.com>
2018-02-23 16:46:08 +0100

    added days left helper method for days left column

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  helper_method :days_left
   protect_from_forgery with: :exception
+
+  def days_left(order)
+    last_day = 9.business_days.after(Date.parse(order.order_date))
+    Date.today.business_days_until(last_day)
+  end
 end
