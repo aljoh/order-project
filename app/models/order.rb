f4c731bc3e43fb25454c8b1c847c40dfed7bfeec
aljoh <reachingalfred@gmail.com>
2018-02-26 13:50:50 +0100

    first state impementation green

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,21 @@
 class Order < ApplicationRecord
   validates :number, :customer, :units, :order_date, presence: true
+
+  before_validation on: :create do |resource|
+    resource.send(:initialize_state_machines, dynamic: :force)
+  end
+
+  state_machine :state, initial: :Arrived do
+    event :produced do
+      transition Arrived: :Ready
+    end
+
+    event :delivered do
+      transition Ready: :Delivered
+    end
+
+    event :order_done do
+      transition Delivered: :Done
+    end
+  end
 end
--- features/order.feature
+++ features/order.feature
@@ -22,5 +22,7 @@ Feature: Visit Homepage
     And I fill in "Order date" with "2018-02-05"
     And I fill in "Note" with "ASAP"
     And I click on "Create Order"
+    And show me the page
     Then I should see "KO02050505"
     And I should see "Order created"
+    And I should see "Arrived"
