d45e0fb94aa58fb91147a1aa4331bb46d1c4f827
aljoh <reachingalfred@gmail.com>
2018-03-02 15:38:34 +0100

    empty header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,26 +13,3 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-function filterTable() {
-  valueNumber = getValue("numberInput");
-  valueCustomer = getValue("customerInput");
-  valueState = getValue("stateInput");
-  table = document.getElementById("orderTable");
-  tr = table.getElementsByTagName("tr");
-  for ( i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td");
-    if (td.length > 0) {
-      if (td[0].innerHTML.toUpperCase().indexOf(valueNumber) > -1 &&
-          (td[1].innerHTML.toUpperCase().indexOf(valueCustomer) > -1 || valueCustomer === 'ALL') &&
-          (td[10].innerHTML.toUpperCase().indexOf(valueState) > -1 || valueState === 'ALL') ) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }
-  }
-}
-
-function getValue(id) {
-  return document.getElementById(id).value.toUpperCase();
-}
